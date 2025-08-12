27a4c65301db001608bbe94196d8bf94e74c6ca3
newman05 <namansurana4566@gmail.com>
2025-08-12 16:20:20 +0530

    refactored

--- Day3/q2.cpp
+++ Day3/q2.cpp
@@ -9,35 +9,33 @@ conclude the input scenario is either best-case partitioning or worst-case parti
 using namespace std;
 
 long long comparisons = 0;
-bool worstCase = false, bestCase = true;
 
-int partitionFunc(vector<int> &arr, int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
-    for (int j = low; j < high; j++) {
-        comparisons++;
-        if (arr[j] <= pivot) {
+int partition(vector<int> &arr, int low, int high) {
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while (i < j) {
+        while (arr[i] <= pivot && i <= high - 1) {
+            comparisons++;
             i++;
-            swap(arr[i], arr[j]);
         }
+        while (arr[j] > pivot && j >= low + 1) {
+            comparisons++;
+            j--;
+        }
+        if (i < j) swap(arr[i], arr[j]);
     }
-    swap(arr[i + 1], arr[high]);
-    int pivotIndex = i + 1;
 
-    int leftSize = pivotIndex - low;
-    int rightSize = high - pivotIndex;
-
-    if (leftSize == 0 || rightSize == 0) worstCase = true;
-    if (abs(leftSize - rightSize) > (high - low) / 2) bestCase = false;
-
-    return pivotIndex;
+    swap(arr[low], arr[j]);
+    return j;
 }
 
-void quickSort(vector<int> &arr, int low, int high) {
+void qs(vector<int> &arr, int low, int high) {
     if (low < high) {
-        int pi = partitionFunc(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
     }
 }
 
@@ -47,26 +45,38 @@ int main() {
     cout << "1. Ascending Data\n";
     cout << "2. Descending Data\n";
     cout << "3. Random Data\n";
-    cout << "4. ERROR (EXIT)\n";
+    cout << "4. EXIT\n";
     cout << "Enter option: ";
     cin >> choice;
 
     vector<int> arr;
-    if (choice == 1) arr = {10, 20, 30, 40, 50, 60, 70};
-    else if (choice == 2) arr = {90, 80, 70, 60, 50, 40, 30};
-    else if (choice == 3) arr = {55, 66, 33, 11, 44, 99, 77};
-    else return 0;
+    int n = 1000;
+
+    if (choice == 1) {
+        arr.resize(n);
+        iota(arr.begin(), arr.end(), 1);
+    }
+    else if (choice == 2) {
+        arr.resize(n);
+        for (int i = 0; i < n; i++)
+            arr[i] = n - i;
+    }
+    else if (choice == 3) {
+        arr.resize(n);
+        srand(time(0));
+        for (int i = 0; i < n; i++)
+            arr[i] = rand();
+    }
+    else
+        return 0;
 
     cout << "Before Sorting: ";
     for (int x : arr) cout << x << " ";
     cout << "\n";
 
     comparisons = 0;
-    worstCase = false;
-    bestCase = true;
-
     auto start = chrono::high_resolution_clock::now();
-    quickSort(arr, 0, arr.size() - 1);
+    qs(arr, 0, arr.size() - 1);
     auto end = chrono::high_resolution_clock::now();
 
     cout << "After Sorting: ";
@@ -74,13 +84,7 @@ int main() {
     cout << "\n";
 
     cout << "Number of Comparisons: " << comparisons << "\n";
-    if (worstCase) cout << "Scenario: Worst-case partitioning\n";
-    else if (bestCase) cout << "Scenario: Best-case partitioning\n";
-    else cout << "Scenario: Average-case partitioning\n";
-
-    cout << "Execution Time: "
-         << chrono::duration_cast<chrono::nanoseconds>(end - start).count()
-         << " nanoseconds\n";
+    cout<<"Execution Time: "<< chrono::duration_cast<chrono::nanoseconds>(end - start).count()<<" nanoseconds\n";
 
     return 0;
 }
